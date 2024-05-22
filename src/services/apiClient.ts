684bb8252b971987e3b1020bf4c239b2ccfb5642
dravidjones <dravidjones2851999@gmail.com>
2024-05-22 14:26:15 +0530

    Changed api key

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -7,7 +7,7 @@ export interface FetchResponse<T> {
 }
 
 // Basic bG9nb3NyZXRyZWF0OlF3ZXIxMjMh
-let apiLink = "https://sleepy-gold-pumps.cyclic.app/api";
+let apiLink = "https://logos-api-1.onrender.com/api";
 // let apiLink = "http://localhost:3000/api";
 
 const axiosInstance = axios.create({
